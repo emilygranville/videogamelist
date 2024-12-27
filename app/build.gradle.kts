4bc3aece8b5ce42245750cb8fb6490ca542d420b
Emily <32372751+emilygranville@users.noreply.github.com>
2024-12-27 16:25:12 -0500

    trying to fix crashes to test (this seems to be the culprit?)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("com.adarshr.test-logger") version "4.0.0"
+    //id("com.adarshr.test-logger") version "4.0.0"
 }
