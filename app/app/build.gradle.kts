212afb325f37fcfdeab4b6423f5e8999ef872638
Emily <32372751+emilygranville@users.noreply.github.com>
2024-12-27 15:28:55 -0500

    adding app components

---
+++ app/app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.emilygranville.videogamelist"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.emilygranville.videogamelist"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    //testImplementation(libs.junit)
+    //testImplementation(libs.junit.jupiter)
+    //androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+//
+//    testImplementation(libs.junit.v5114)
+//    androidTestImplementation(libs.junit.v5114)
+}
