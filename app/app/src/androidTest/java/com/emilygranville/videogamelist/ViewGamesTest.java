0bde9eb2af8bc9646e7fd80f4c001e8fd4fe2c92
Emily <32372751+emilygranville@users.noreply.github.com>
2025-03-20 17:08:03 -0400

    Cleaned up first Espresso test with a variable

--- app/app/src/androidTest/java/com/emilygranville/videogamelist/ViewGamesTest.java
+++ app/app/src/androidTest/java/com/emilygranville/videogamelist/ViewGamesTest.java
@@ -1,6 +1,7 @@
 package com.emilygranville.videogamelist;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
@@ -9,6 +10,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -21,21 +23,20 @@ public class ViewGamesTest {
 
     @org.junit.Test
     public void viewGames() {
+        ViewInteraction rv = onView(withId(R.id.displayRecyclerView));
+
         // is the recycler view showing anything
-        onView(withId(R.id.displayRecyclerView))
-                .check(matches(isDisplayed()));
+        rv.check(matches(isDisplayed()));
         // does it have 4 children
         // based on shown items only
-        onView(withId(R.id.displayRecyclerView))
-                .check(matches(hasMinimumChildCount(4)));
+        rv.check(matches(hasMinimumChildCount(4)));
 
         // does it display game1
-        onView(ViewMatchers.withId(R.id.displayRecyclerView))
-                .check(matches(hasDescendant(withText("Game1"))));
+        rv.check(matches(hasDescendant(withText("Game1"))));
         // scroll to game 6
         // then does it display game6
-        onView(withId(R.id.displayRecyclerView))
-                .perform(scrollToPosition(5))
+        rv.perform(scrollToPosition(5))
                 .check(matches(hasDescendant(withText("Game6"))));
     }
+
 }
