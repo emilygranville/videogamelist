5fc1ffaacd5e02240bec4350e3dc0024a54334a2
Emily <32372751+emilygranville@users.noreply.github.com>
2025-07-01 17:20:35 -0400

    Updating target SDK

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.emilygranville.videogamelist"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
