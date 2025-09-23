2f1f7e366b364152917622dbb671d5d3b9fb4eaa
Emily <32372751+emilygranville@users.noreply.github.com>
2025-09-23 15:47:25 -0400

    creating testing data

---
+++ website/video-game-list/src/test-data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        gameID: 0,
+        gameName: "Game 0",
+        isFavorite: true,
+        price: 0,
+        consoles: [
+            "CONSOLE 1",
+            "CONSOLE 2"
+        ]
+    },
+    {
+        gameID: 1,
+        gameName: "Game 1",
+        isFavorite: true,
+        price: 0,
+        consoles: [
+            "CONSOLE 1",
+            "CONSOLE 2",
+            "CONSOLE 0"
+        ]
+    }
+]
